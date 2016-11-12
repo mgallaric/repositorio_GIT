3e962dbab08c0c4f7074988e98327b1da5905e2e
mgallaric <sdfsdfsd>
2016-11-12 12:44:27 +0100

    Ejercicio de Spring

---
+++ Ejercicio1/src/ejercicio1/Saludo.java
@@ -0,0 +1,18 @@
+package ejercicio1;
+
+public class Saludo {
+
+    private String mensaje;
+
+    public Saludo() {
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+}
