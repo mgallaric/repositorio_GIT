9b2dbaf969a4490f84415622ef5087f45daa266a
Portatil <Portatil@Portatil-PC>
2016-11-12 13:44:43 +0100

    Ejercicio 4 Spring

---
+++ Ejercicio4/src/ejercicio4/Colegio.java
@@ -0,0 +1,30 @@
+package ejercicio4;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class Colegio {
+
+    private Collection<String> alumnos;
+    private Map<String,String> alumnos2;
+
+    public Collection<String> getAlumnos() {
+        return alumnos;
+    }
+
+    public void setAlumnos(Collection<String> alumnos) {
+        this.alumnos = alumnos;
+    }
+
+    public Map<String, String> getAlumnos2() {
+        return alumnos2;
+    }
+
+    public void setAlumnos2(Map<String, String> alumnos2) {
+        this.alumnos2 = alumnos2;
+    }
+
+    
+    
+
+}
